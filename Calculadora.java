b6c2bddd7a6b1571592d15570a66f8dad508335c
Error404-NotFound_ <Arpachelli@gmail.com>
2026-02-27 22:50:34 -0300

    Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -2,25 +2,43 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class Calculadora{
+    Scanner leitor = new Scanner(System.in);
     public static void main(String[] args){
-        Scanner leitor = new Scanner(System.in);
         System.out.println("---Calculadora Java---");
-
-    public double n1(double n1)
-    try{
-        double n1 = leitor.nextDouble();}
-    catch (InputMismatchException e){
-        System.out.print("Utilize apenas vírgulas para casas decimais.");}
-        
+    }
+    public double n1(double n1){
+        try{
+            
+            double n1 = leitor.nextDouble();
+            return n1;
+        }
+        catch (InputMismatchException e){
+            System.out.print("Utilize apenas vírgulas para casas decimais.");
+            return;
+        }
+    }
 
 
     String equacao = leitor.next();
-    if not (+, -, *, /) in equacao
+    if (!"+-*/".contains(equacao)){
+        System.out.println("Operador inválido!");
+        return;
+    }
+    else {
+        continue;
+    }
 
-    try{
-        double n2 = leitor.nextDouble();}
-    catch (InputMismatchException e){
-        System.out.print("Utilize apenas vírgulas para casas decimais.");}
+    public double n2(double n2){
+        try{
+            double n2 = leitor.nextDouble();
+            return n2;
+        }
+        catch (InputMismatchException e){
+            System.out.print("Utilize apenas vírgulas para casas decimais.");
+            return;
+        }
     }
 }
+
+
 
