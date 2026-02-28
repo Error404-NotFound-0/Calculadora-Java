df36237091d6f3b052d9a81268fdcbaec141035f
Error404-NotFound_ <Arpachelli@gmail.com>
2026-02-27 22:09:47 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Calculadora{
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("---Calculadora Java---");
+
+    public double n1(double n1)
+    try{
+        double n1 = leitor.nextDouble();}
+    catch (InputMismatchException e){
+        System.out.print("Utilize apenas vírgulas para casas decimais.");}
+        
+
+
+    String equacao = leitor.next();
+    if not (+, -, *, /) in equacao
+
+    try{
+        double n2 = leitor.nextDouble();}
+    catch (InputMismatchException e){
+        System.out.print("Utilize apenas vírgulas para casas decimais.");}
+    }
+}
+
