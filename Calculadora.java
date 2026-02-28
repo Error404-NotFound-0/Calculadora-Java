615eaff4591d85a5b9ca0c57270c11356529193c
Error404-NotFound_ <Arpachelli@gmail.com>
2026-02-28 14:00:54 -0300

    Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,44 +1,55 @@
-import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.Scanner;
 
-public class Calculadora{
-    Scanner leitor = new Scanner(System.in);
-    public static void main(String[] args){
+public class Calculadora {
+    
+    static Scanner leitor = new Scanner(System.in);
+
+    public static void main(String[] args) {
         System.out.println("---Calculadora Java---");
+        
+        
+        double primeiro = n1(); 
+        String op = equacao();
+        double segundo = n2();
+
+        System.out.println("Resultado: " + primeiro + " " + op + " " + segundo);
     }
-    public double n1(double n1){
-        try{
-            
-            double n1 = leitor.nextDouble();
-            return n1;
-        }
-        catch (InputMismatchException e){
-            System.out.print("Utilize apenas vírgulas para casas decimais.");
-            return;
+
+    
+    public static double n1() {
+        try {
+            System.out.print("Insira o primeiro número: ");
+            return leitor.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: Utilize apenas vírgulas para casas decimais.");
+            leitor.next(); 
+            n1();
+            return 0;
         }
     }
 
-
-    String equacao = leitor.next();
-    if (!"+-*/".contains(equacao)){
-        System.out.println("Operador inválido!");
-        return;
-    }
-    else {
-        continue;
-    }
-
-    public double n2(double n2){
-        try{
-            double n2 = leitor.nextDouble();
-            return n2;
+    
+    public static String equacao() {
+        System.out.print("Digite a operação (+, -, *, /): ");
+        String operacao = leitor.next();
+        
+        if (!"+-*/".contains(operacao)) {
+            System.out.println("Operador inválido!");
+            equacao();
         }
-        catch (InputMismatchException e){
-            System.out.print("Utilize apenas vírgulas para casas decimais.");
-            return;
+        return operacao;
+    }
+
+    public static double n2() {
+        try {
+            System.out.print("Insira o segundo número: ");
+            return leitor.nextDouble();
+        } catch (InputMismatchException e) {
+            System.out.println("Erro: Utilize apenas vírgulas para casas decimais.");
+            leitor.next();
+            n2();
+            return 0;
         }
     }
 }
-
-
-
